b1808fe7270ca27b1ea534f4d3e917137e5459d1
Aaron Chakerian <achakerian@gmail.com>
2024-10-04 17:19:36 +1000

    fresh

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Function with type annotations for parameters and return value
-function greet(name) {
-    return `Hello, ${name}!`;
-}
-// Call the function
-const userName = "Alice";
-console.log(greet(userName));
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-
-// Function with type annotations for parameters and return value
-function greet(name: string): string {
-    return `Hello, ${name}!`;
-}
-
-// Call the function
-const userName: string = "Alice";
-console.log(greet(userName));
